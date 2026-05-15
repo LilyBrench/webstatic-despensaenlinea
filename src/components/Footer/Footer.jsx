fb621db85d48804c67c719a48bdfc8c27edc5f4a
LilyBrench <liliproyecto04@gmail.com>
2026-05-15 17:21:36 -0600

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="social-icons">
         <a href="https://www.facebook.com/DespensaEnLinea/" target="_blank" rel="noopener noreferrer">
-          <img src="/img/facebook.png" alt="Facebook" />
+          <img src="/facebook.png" alt="Facebook" />
         </a>
         <a href="https://instagram.com/despensaenlinea.mx/" target="_blank" rel="noopener noreferrer">
           <img src="/img/img1/instagram.png" alt="Instagram" />
