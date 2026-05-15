813ed36ec2b0d57bbe9e97ad06ab075cc3431a8e
LilyBrench <liliproyecto04@gmail.com>
2026-05-15 17:42:30 -0600

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
           <img src="/facebook.png" alt="Facebook" />
         </a>
         <a href="https://instagram.com/despensaenlinea.mx/" target="_blank" rel="noopener noreferrer">
-          <img src="/img/img1/instagram.png" alt="Instagram" />
+          <img src="/instagram.png" alt="Instagram" />
         </a>
         <a href="https://wa.me/525544972959" target="_blank" rel="noopener noreferrer">
-          <img src="/img/img1/whatsapp (1).png" alt="WhatsApp" />
+          <img src="/whatsapp (1).png" alt="WhatsApp" />
         </a>
       </div>
       <p className="copyright">
